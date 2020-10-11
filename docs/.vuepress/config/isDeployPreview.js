066b1d101e527907612da7c9a31fbdb4561eb6c6
Alvaro Saburido <alvaro.saburido@gmail.com>
2020-10-11 08:35:02 +0200

    fix(docs): remove local docs since are moved to a another repo

--- docs/.vuepress/config/isDeployPreview.js
+++
@@ -1,1 +0,0 @@
-module.exports = process.env.hasOwnProperty('DEPLOY_PREVIEW');
--- docs/.vuepress/config/meta.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  title: 'Vue Dynamic Forms',
-  description:
-    'Easy way to dynamically create reactive forms in vue based on varying business object model.',
-  // url: 'https://vue-select.org',
-  icon: '/vue-dynamic-forms.png',
-};
